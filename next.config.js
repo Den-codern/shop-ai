8ee25703b79b8a8fb956b5f47984fd544817e0bf
Den-codern <78246895+Den-codern@users.noreply.github.com>
2022-03-26 23:05:50 +0600

    env file

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
   images: {
 		domains: ['cdn.pixabay.com','drop.ndtv.com','m.media-amazon.com']
 	},
-  env: {
-    BASE_URL: process.env.PORT,
-  },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
